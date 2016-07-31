e4bae1f73fcc5e4e93edecb84c7dd931e39bcc08
Maksim <maks.mephi@gmail.com>
2016-07-31 22:17:02 +0300

    revert back API keys

--- settings.py
+++ settings.py
@@ -1,11 +1,2 @@
-"""
-client_id - 'Your client_id from https://stepic.org/oauth2/applications/'
-client_secret - 'Your client_secret from https://stepic.org/oauth2/applications/'
-"""
-
-import os
-
-client_id = os.environ.get('STEPIC_CLIENT_ID')
-client_secret = os.environ.get('STEPIC_CLIENT_SECRET')
-
-# print("client_id = {}\nclient_secret={}".format(client_id, client_secret))
+client_id = 'Your client_id from https://stepic.org/oauth2/applications/'
+client_secret = 'Your client_secret from https://stepic.org/oauth2/applications/'
