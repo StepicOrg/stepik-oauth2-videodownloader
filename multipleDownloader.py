252fea6e1faa2617e9c2bf9e96dd886fbb334eea
Maksim <maks.mephi@gmail.com>
2016-07-31 23:35:55 +0300

    rename "main.py" to "multipleDownloader.py"; use courses and quality from the settings file; delete folder creation

--- multipleDownloader.py
+++ multipleDownloader.py
@@ -7,30 +7,21 @@ Run by:
 python3 main.py
 
 Additional notes:
-1) Add courses which you want to download to the "courses" dictionary.
+1) Add courses which you want to download to the "settings.courses" dictionary.
 
-2) You could also set quality of the video by changing "quality" variable. 
+2) You could also set quality of the video by changing "settings.quality" variable.
 The default quality is 720.
 """
 
 import os
 
 import weekDownloader
+import settings
+
 
 def main():
-    # Set the quality of a video.
-    quality = '720'
-
-    courses = {}
-
-    # Add courses.
-    # courses[course_id] = [week_1, week_2, ...]
-
-    # Course example.
-    courses["154"] = ["1", "2", "3"]
-
     # Loop over courses.
-    for key, value in courses.items():
+    for key, value in settings.courses.items():
         course_id = key
         course_weeks = value
 
@@ -42,25 +33,15 @@ def main():
 
             folder_name = course_id + os.sep + week_id
 
-            try:
-                # Create a directory for a particular week in the course.
-                os.makedirs(folder_name)
-            except PermissionError:
-                print("Run the script from admin")
-                exit(1)
-            except FileExistsError:
-                print("Please delete the folder " + folder_name)
-                exit(1)
-
             argv.append(folder_name)
 
-            argv.append(quality)
+            argv.append(settings.quality)
 
             print("argv: {}".format(argv))
 
             try:
                 # Download all videos in a particular week in the course.
-                weekDownloader.batch_main(argv)
+                weekDownloader.main(argv)
             except (KeyError, IndexError) as E:
                 print("! Error with course: ", "'" + folder_name + "',", "ErrorType: ", E)
                 continue
