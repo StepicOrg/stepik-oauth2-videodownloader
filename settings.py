081f5f3951eb4a2fc3b3a6f49d12e04b7d81dff3
Maksim <maks.mephi@gmail.com>
2016-07-31 23:28:57 +0300

    add "quality" and "courses" parameters for multipleDownloader

--- settings.py
+++ settings.py
@@ -1,2 +1,20 @@
 client_id = 'Your client_id from https://stepic.org/oauth2/applications/'
 client_secret = 'Your client_secret from https://stepic.org/oauth2/applications/'
+
+# Set the quality of a video for multipleDownloader.
+#
+# You could still use command line interface with weekDownloader!
+#
+# If the is no required video quality then download
+# with the best available quality.
+quality = '720'
+
+courses = {}
+
+# Add courses for multipleDownloader.
+# You could still use command line interface with weekDownloader!
+#
+# courses[course_id] = [week_1, week_2, ...]
+
+# Course example.
+courses['154'] = ['1', '2', '3']
